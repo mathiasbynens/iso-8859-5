aa371216bab959b8f1621564b74ad79cd5f6456a
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:52:31 +0200

    Tag the v0.1.2 release

--- iso-8859-5.js
+++ iso-8859-5.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/iso-8859-5 v0.1.1 by @mathias | MIT license */
+/*! http://mths.be/iso-8859-5 v0.1.2 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -125,7 +125,7 @@
 			'iso_8859-5',
 			'iso_8859-5:1988'
 		],
-		'version': '0.1.1'
+		'version': '0.1.2'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
